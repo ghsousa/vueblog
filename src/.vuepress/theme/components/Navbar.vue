b71017e55b0dd60eee831080dc5ef2fd229eb497
Gabriel Sousa <gsousa09@gmail.com>
2020-01-02 09:11:46 -0500

    Change sitename font size

--- src/.vuepress/theme/components/Navbar.vue
+++ src/.vuepress/theme/components/Navbar.vue
@@ -124,7 +124,7 @@ $navbar-horizontal-padding = 1.5rem
     margin-right 0.8rem
     vertical-align top
   .site-name
-    font-size 1.3rem
+    font-size 1.30rem
     font-weight 600
     position relative
   .links
